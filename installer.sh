61783416ef74e9e375c859a050cef26eda7cc756
AlphaBs <ksi123456ab@naver.com>
2021-10-21 06:37:53 +0900

    installer.sh 스크립트 작성

---
+++ installer.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+mkdir ./installer
+cp ./bin/app.sh ./installer
+cp ./docker-compose.yml ./installer
+mkdir -p ./installer/volume/in
+mkdir -p ./installer/volume/out
+cp ./volume/nginx.conf ./installer/volume
+
+tar czf installer.tar.gz ./installer
+rm -r ./installer
+echo Done.
