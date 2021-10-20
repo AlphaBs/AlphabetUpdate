759b16c2497685717be78360b5fe962113f33001
권세인 <ksi123456ab@naver.com>
2021-10-20 17:20:29 +0900

    move to src directory

--- AlphabetUpdateServer/Models/LoginModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AlphabetUpdateServer.Models
-{
-    public class LoginModel
-    {
-        public string? Name { get; init; }
-        public string? Host { get; init; }
-        public string? Password { get; init; }
-    }
-}
--- AlphabetUpdateServerInstaller/ServerPassword.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AlphabetUpdateServerInstaller
-{
-    public record ServerPassword
-    {
-        public string RawPassword { get; init; }
-        public string AesKey { get; init; }
-        public string AesIv { get; init; }
-    }
-}
