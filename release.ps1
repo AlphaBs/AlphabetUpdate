f8a1869a27df47d29f588bc842a51027f4a1f13e
AlphaBs <ksi123456ab@naver.com>
2021-11-09 22:26:24 +0900

    update release script

--- release.ps1
+++ release.ps1
@@ -14,20 +14,20 @@ mkdir release\netstandard2.1\AlphabetUpdate.Client
 mkdir release\netstandard2.1\AlphabetUpdate.Common
 mkdir release\netstandard2.1\AuthYouClient
 
-Copy-Item .\AlphabetUpdate.Client\bin\Release\AlphabetUpdate.Client.$ver.nupkg .\release
-Copy-Item .\AlphabetUpdate.Common\bin\Release\AlphabetUpdate.Common.$ver.nupkg .\release
-Copy-Item .\AuthYouClient\bin\Release\AuthYouclient.$ver.nupkg .\release
+Copy-Item .\src\AlphabetUpdate.Client\bin\Release\AlphabetUpdate.Client.$ver.nupkg .\release
+Copy-Item .\src\AlphabetUpdate.Common\bin\Release\AlphabetUpdate.Common.$ver.nupkg .\release
+Copy-Item .\src\AuthYouClient\bin\Release\AuthYouclient.$ver.nupkg .\release
 
 nuget add .\release\AlphabetUpdate.Client.$ver.nupkg -Source C:\p\nuget\
 nuget add .\release\AlphabetUpdate.Common.$ver.nupkg -Source C:\p\nuget\
 nuget add .\release\AuthYouClient.$ver.nupkg -Source C:\p\nuget\
 
-Copy-Item .\AlphabetUpdate.Client\bin\Release\net462\* .\release\net462\AlphabetUpdate.Client\
-Copy-Item .\AlphabetUpdate.Common\bin\Release\net462\* .\release\net462\AlphabetUpdate.Common\
-Copy-Item .\AuthYouClient\bin\Release\net462\* .\release\net462\AuthYouClient\
+Copy-Item .\src\AlphabetUpdate.Client\bin\Release\net462\* .\release\net462\AlphabetUpdate.Client\
+Copy-Item .\src\AlphabetUpdate.Common\bin\Release\net462\* .\release\net462\AlphabetUpdate.Common\
+Copy-Item .\src\AuthYouClient\bin\Release\net462\* .\release\net462\AuthYouClient\
 
-Copy-Item .\AlphabetUpdate.Client\bin\Release\netstandard2.1\* .\release\netstandard2.1\AlphabetUpdate.Client\
-Copy-Item .\AlphabetUpdate.Common\bin\Release\netstandard2.1\* .\release\netstandard2.1\AlphabetUpdate.Common\
-Copy-Item .\AuthYouClient\bin\release\netstandard2.1\* .\release\netstandard2.1\AuthYouClient\
+Copy-Item .\src\AlphabetUpdate.Client\bin\Release\netstandard2.1\* .\release\netstandard2.1\AlphabetUpdate.Client\
+Copy-Item .\src\AlphabetUpdate.Common\bin\Release\netstandard2.1\* .\release\netstandard2.1\AlphabetUpdate.Common\
+Copy-Item .\src\AuthYouClient\bin\release\netstandard2.1\* .\release\netstandard2.1\AuthYouClient\
 
 pause
