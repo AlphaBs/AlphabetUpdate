27c2a4602c6ff584276961a9214599259c058e15
AlphaBs <ksi123456ab@naver.com>
2021-11-19 03:59:05 +0900

    add AuthYouExtensions

---
+++ src/AuthYouClient/AuthYouExtensions.cs
@@ -0,0 +1,30 @@
+﻿using AlphabetUpdate.Client;
+using AuthYouClient.ProcessInteractor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthYouClient
+{
+    public static class AuthYouExtensions
+    {
+        public static void AddAuthYouInteractor(this LauncherCoreBuilder builder, AuthYouProcessInteractor interactor)
+        {
+            builder.AddProcessInteractor(interactor);
+        }
+
+        public static void AddAuthYouInteractor(this LauncherCoreBuilder builder, AuthYouClientCore client)
+        {
+            builder.AddProcessInteractor(new AuthYouProcessInteractor(client));
+        }
+
+        public static void AddAuthYouInteractor(this LauncherCoreBuilder builder, AuthYouClientSettings clientSettings)
+        {
+            builder.AddProcessInteractor(
+                new AuthYouProcessInteractor(
+                    new AuthYouClientCore(clientSettings)));
+        }
+    }
+}
