3840b21a90eca76f0f4c044c2829c6d623dde649
AlphaBs <ksi123456ab@naver.com>
2021-11-19 04:20:24 +0900

    add OptionsPatchHandler and ShaderOptionsPatchHandler

--- src/AlphabetUpdate.Client/PatchHandler/OptionsPatchHandler.cs
+++ src/AlphabetUpdate.Client/PatchHandler/OptionsPatchHandler.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using CmlLib.Core;
 using CmlLib.Core.Downloader;
@@ -14,120 +15,37 @@ namespace AlphabetUpdate.Client.PatchHandler
     {
         private static readonly ILog log = LogManager.GetLogger(nameof(OptionsPatchHandler));
         
-        public OptionsPatchHandler()
+        public OptionsPatchHandler(GameOptions opts)
         {
-            
+            this.gameOptions = opts;
         }
+
+        private readonly GameOptions gameOptions;
 
         public event DownloadFileChangedHandler? FileChanged;
         public event ProgressChangedEventHandler? ProgressChanged;
-        public async Task Patch(PatchContext context)
+
+        public Task Patch(PatchContext context)
         {
-            
-        }
-        
-        private void SetOptionFiles(MinecraftPath path, Dictionary<string, string> tagFileName)
-        {
-            if (tagFileName.Count > 0)
-            {
-                string res;
-                if (tagFileName.TryGetValue("res_", out res))
-                {
-                    SetResourcePack(path, res);
-                }
+            var optionPath = Path.Combine(context.MinecraftPath.BasePath, "options.txt");
 
-                string shader;
-                if (tagFileName.TryGetValue("shader_", out shader))
-                {
-                    shader = Path.GetFileName(shader);
-                    log.Info("optionsshaders.txt, " + shader);
-                    var shaderPath = Path.Combine(path.BasePath, "optionsshaders.txt");
-                    var shaderoptions = GetShaderOptions(shaderPath);
-
-                    shaderoptions["shaderPack"] = shader;
-
-                    SaveShaderOptions(shaderPath, shaderoptions);
-                }
-            }
-        }
-
-        private void SetResourcePack(MinecraftPath path, string res)
-        {
-            res = Path.GetFileName(res);
-            log.Info("options.txt " + res);
-
-            var optionPath = Path.Combine(path.BasePath, "options.txt");
-            var gameoptions = GetOptions(optionPath);
-            gameoptions?.SetRawValue("resourcePacks", $"[\"{res}\"]");
-            gameoptions?.Save();
-        }
-
-        private GameOptionsFile GetOptions(string optionPath)
-        {
-            GameOptionsFile gameoptions;
-
+            GameOptionsFile optionFile;
             if (File.Exists(optionPath))
-                gameoptions = GameOptionsFile.ReadFile(optionPath);
+                optionFile = GameOptionsFile.ReadFile(optionPath);
             else
-                gameoptions = null;
-            return gameoptions;
+                optionFile = new GameOptionsFile();
+
+            if (gameOptions.ResourcePacks != null)
+                addResourcePacks(optionFile, gameOptions.ResourcePacks);
+
+            optionFile.Save();
+            return Task.CompletedTask;
         }
 
-        private Dictionary<string, string> GetShaderOptions(string optionPath)
+        private void addResourcePacks(GameOptionsFile options, string[] names)
         {
-            Dictionary<string, string> options = new Dictionary<string, string>();
-            if (File.Exists(optionPath))
-            {
-                using (var sr = new StreamReader(File.OpenRead(optionPath)))
-                {
-                    string line;
-                    while ((line = sr.ReadLine()) != null)
-                    {
-                        if (string.IsNullOrEmpty(line))
-                            continue;
-
-                        var spl = line.Split('=');
-
-                        var key = spl[0];
-                        string value = null;
-
-                        if (spl.Length > 1)
-                        {
-                            var values = new string[spl.Length - 1];
-
-                            Array.Copy(spl, 1, values, 0, spl.Length - 1);
-                            value = string.Join("=", values);
-                        }
-
-                        options[key] = value;
-                    }
-                }
-            }
-
-            return options;
-        }
-
-        private void SaveShaderOptions(string optionPath, Dictionary<string, string> options)
-        {
-            try
-            {
-                using (var sw = new StreamWriter(File.OpenWrite(optionPath)))
-                {
-                    foreach (KeyValuePair<string, string> item in options)
-                    {
-                        string line = item.Key;
-                        if (item.Value != null)
-                            line += "=" + item.Value;
-
-                        sw.WriteLine(line);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                log.Info("SaveShaderOptions Exception");
-                log.Info(ex);
-            }
+            var rawValue = string.Join(",", names.Select(name => $"\"{Path.GetFileName(name)}\""));
+            options?.SetRawValue("resourcePacks", $"[{rawValue}]");
         }
     }
 }
---
+++ src/AlphabetUpdate.Client/PatchHandler/ShaderOptionsPatchHandler.cs
@@ -0,0 +1,117 @@
+﻿using CmlLib.Core;
+using CmlLib.Core.Downloader;
+using log4net;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlphabetUpdate.Client.PatchHandler
+{
+    public class ShaderOptionsPatchHandler : IPatchHandler
+    {
+        private static readonly ILog log = LogManager.GetLogger(nameof(OptionsPatchHandler));
+
+        public ShaderOptionsPatchHandler(ShaderOptions opts)
+        {
+            this.shaderOptions = opts;
+        }
+
+        private readonly ShaderOptions shaderOptions;
+
+        public event DownloadFileChangedHandler? FileChanged;
+        public event ProgressChangedEventHandler? ProgressChanged;
+
+        public Task Patch(PatchContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        // legacy codes: 
+
+        //private void SetOptionFiles(MinecraftPath path, Dictionary<string, string> tagFileName)
+        //{
+        //    if (tagFileName.Count > 0)
+        //    {
+        //        string res;
+        //        if (tagFileName.TryGetValue("res_", out res))
+        //        {
+        //            //SetResourcePack(path, res);
+        //        }
+
+        //        string shader;
+        //        if (tagFileName.TryGetValue("shader_", out shader))
+        //        {
+        //            shader = Path.GetFileName(shader);
+        //            log.Info("optionsshaders.txt, " + shader);
+        //            var shaderPath = Path.Combine(path.BasePath, "optionsshaders.txt");
+        //            var shaderoptions = GetShaderOptions(shaderPath);
+
+        //            shaderoptions["shaderPack"] = shader;
+
+        //            SaveShaderOptions(shaderPath, shaderoptions);
+        //        }
+        //    }
+        //}
+
+        private Dictionary<string, string?> GetShaderOptions(string optionPath)
+        {
+            var options = new Dictionary<string, string?>();
+            if (File.Exists(optionPath))
+            {
+                using (var sr = new StreamReader(File.OpenRead(optionPath)))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        if (string.IsNullOrEmpty(line))
+                            continue;
+
+                        var spl = line.Split('=');
+
+                        var key = spl[0];
+                        string? value = null;
+
+                        if (spl.Length > 1)
+                        {
+                            var values = new string[spl.Length - 1];
+
+                            Array.Copy(spl, 1, values, 0, spl.Length - 1);
+                            value = string.Join("=", values);
+                        }
+
+                        options[key] = value;
+                    }
+                }
+            }
+
+            return options;
+        }
+
+        private void SaveShaderOptions(string optionPath, Dictionary<string, string> options)
+        {
+            try
+            {
+                using (var sw = new StreamWriter(File.OpenWrite(optionPath)))
+                {
+                    foreach (KeyValuePair<string, string> item in options)
+                    {
+                        string line = item.Key;
+                        if (item.Value != null)
+                            line += "=" + item.Value;
+
+                        sw.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                log.Info("SaveShaderOptions Exception");
+                log.Info(ex);
+            }
+        }
+    }
+}
