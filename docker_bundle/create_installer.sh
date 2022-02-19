9dea36e3baf90d20c51ece77fec0a038cfa308a1
AlphaBs <ksi123456ab@naver.com>
2022-02-19 23:50:33 +0900

    update docker

--- docker_bundle/create_installer.sh
+++ docker_bundle/create_installer.sh
@@ -9,8 +9,10 @@ cp ./docker-compose.yml ./app
 mkdir -p ./app/volume/in
 mkdir -p ./app/volume/out
 mkdir -p ./app/volume/vnstat
-cp ./nginx.conf ./app/volume
-cp ./appsettings.json ./app/volume
+mkdir -p ./app/volume/nginx
+cp ./volume/nginx.conf ./app/volume
+cp ./volume/appsettings.json ./app/volume
+cp -r ./volume/nginx ./app/volume
 
 tar -czf app.tar.gz ./app/*
 rm -r ./app
